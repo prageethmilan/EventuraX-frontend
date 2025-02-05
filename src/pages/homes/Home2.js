11ff9dbc4ea0e178b9fceae6761c8c7fb468b343
prageethmilan <prageethmilan1999@gmail.com>
2025-02-05 09:31:12 +0530

    remove categories section

--- src/pages/homes/Home2.js
+++ src/pages/homes/Home2.js
@@ -31,47 +31,8 @@ function Home2() {
             {/* Banner */}
             <BannerTwo bgImg={sectiondata.herobanners.banner2.bgimage} />
 
-            {/* Popular Destination */}
-            <section className="cat-area destination-area padding-top-100px padding-bottom-100px">
-                <div className="container">
-                    <div className="row section-title-width text-center mb-4">
-                        <SectionsHeading title={sectiondata.populardestination.sectitle}
-                                         desc={sectiondata.populardestination.seccontent}/>
-                    </div>
-                        <div className="container">
-                            <div className="row">
-                                <PopularCategories catitems={sectiondata.popularcategories.categories}/>
-                                <PopularCategoriesMore catitems={sectiondata.popularcategories.morecats}/>
-                            </div>
-                        </div>
-                </div>
-            </section>
-
             <SectionDivider/>
 
-            {/*/!* Recommended Place *!/*/}
-            {/*<section className="card-area padding-top-100px padding-bottom-90px text-center">*/}
-            {/*    <div className="container">*/}
-            {/*        <div className="row section-title-width text-center">*/}
-            {/*            <SectionsHeading title={sectiondata.recommendedplaces.sectitle}*/}
-            {/*                             desc={sectiondata.recommendedplaces.seccontent} />*/}
-            {/*        </div>*/}
-            {/*        <RecommendedPlace recommendplaces={sectiondata.recommendedplaces.items} />*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/* FunFacts */}
-            {/*<section className="funfact-area section-bg-2 padding-top-100px padding-bottom-50px text-center">*/}
-            {/*    <div className="container">*/}
-            {/*        <div className="row section-title-width text-center">*/}
-            {/*            <SectionsHeading title={sectiondata.funfacts.funfact2.sectitle} titleClass="text-white" desc={sectiondata.funfacts.funfact2.seccontent} />*/}
-            {/*        </div>*/}
-
-            {/*        <FunFactsTwo funfactitems={sectiondata.funfacts.funfact2.counteritems} />*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/* How It Work */}
             <section className="hiw-area padding-top-100px padding-bottom-80px after-none text-center">
                 <div className="container">
                     <div className="row section-title-width text-center">
@@ -82,63 +43,8 @@ function Home2() {
                 </div>
             </section>
 
-            {/* Authors */}
-            {/*<section className="author-area padding-top-100px padding-bottom-100px">*/}
-            {/*    <div className="container-fluid">*/}
-            {/*        <div className="row section-title-width text-center">*/}
-            {/*            <SectionsHeading title={sectiondata.authors.sectitle} titleClass="text-white" desc={sectiondata.authors.seccontent} />*/}
-            {/*        </div>*/}
-
-            {/*        <Authors authoritems={sectiondata.authors.sliders} />*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/* Testimonial */}
-            {/*<section className="testimonial-area padding-top-100px padding-bottom-100px text-center">*/}
-            {/*    <div className="container">*/}
-            {/*        <div className="row section-title-width text-center">*/}
-            {/*            <SectionsHeading title={sectiondata.testimonialdata.sectitle} desc={sectiondata.testimonialdata.seccontent} />*/}
-            {/*        </div>*/}
-            {/*        <div className="row">*/}
-            {/*            <div className="col-lg-8 mx-auto mt-4">*/}
-            {/*                <Testimonial slideitems={sectiondata.testimonialdata.sliders} />*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/*<SectionDivider />*/}
-
-            {/* Blog */}
-            {/*<section className="blog-area padding-top-100px padding-bottom-80px">*/}
-            {/*    <div className="container">*/}
-            {/*        <div className="row section-title-width section-title-ml-mr-0">*/}
-            {/*            <div className="col-lg-8">*/}
-            {/*                <SectionsHeading title={sectiondata.latestarticles.sectitle} desc={sectiondata.latestarticles.seccontent} />*/}
-            {/*            </div>*/}
-            {/*            <div className="col-lg-4">*/}
-            {/*                <div className="btn-box h-100 d-flex align-items-center justify-content-end">*/}
-            {/*                    <Button text={sectiondata.latestarticles.btntext} url={sectiondata.latestarticles.btnurl} className=" margin-top-100px" />*/}
-            {/*                </div>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-
-            {/*        <LatestBlog latestarticles={sectiondata.latestarticles.items} />*/}
-            {/*    </div>*/}
-            {/*</section>*/}
-
-            {/* CTA 2 */}
-            {/*<section className="cta-area cta-area3 padding-top-100px padding-bottom-100px section-bg">*/}
-            {/*    <CtaOne />*/}
-            {/*</section>*/}
-
-            {/* Client Logo */}
-            {/*<ClientLogo logos={sectiondata.clientlogos} />*/}
-
-            {/* NewsLetter */}
             <NewsLetter newsLetterContent={sectiondata.calltoactions.newsletters} />
 
-            {/* Footer */}
             <Footer />
 
             <ScrollTopBtn />

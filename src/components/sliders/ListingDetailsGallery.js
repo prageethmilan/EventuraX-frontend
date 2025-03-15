d1904a89842edf776f249b1df0014c61a1fa30a0
prageethmilan <prageethmilan1999@gmail.com>
2025-03-15 21:59:42 +0530

    updated and removed unwanted images

--- src/components/sliders/ListingDetailsGallery.js
+++ src/components/sliders/ListingDetailsGallery.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import Slider from "react-slick";
-import dotimg1 from '../../assets/images/g-img1.jpg';
 
 
 import 'slick-carousel/slick/slick.css'
@@ -13,8 +12,7 @@ function ListingDetailsGallery({data}) {
     const [slider1, setSlider1] = useState(null);
     const [slider2, setSlider2] = useState(null);
     const [state, setState] = useState({
-        items: [],
-        slideDots: []
+        items: []
     })
 
     useEffect(() => {
@@ -26,21 +24,16 @@ function ListingDetailsGallery({data}) {
 
     useEffect(() => {
         const images = []
-        const slideDots = []
         if (data?.images.length > 0) {
             data?.images.map(item => {
                 images.push({
                     img: item
-                })
-                slideDots.push({
-                    img: dotimg1
                 })
             })
         }
 
         const obj = {
             items: images,
-            slideDots: slideDots
         }
 
         setState(obj)
@@ -48,44 +41,13 @@ function ListingDetailsGallery({data}) {
 
 
     const settingsMain = {
-        slidesToShow: state?.items.length > 1 ? 1 : state.items.length,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        infinite: state?.items.length > 1,
-        arrows: false,
+        infinite: true,
+        arrows: true,
+        dots: true,
         asNavFor: '.slider-nav',
         className: 'places-carousel gallery-carousel padding-top-35px'
-    };
-    const settingsThumbs = {
-        slidesToShow: Math.min(state?.slideDots.length, 6),
-        slidesToScroll: 1,
-        infinite: state?.slideDots.length > 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        swipeToSlide: true,
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 700,
-                settings: {
-                    arrows: false,
-                    slidesToShow: Math.min(state?.slideDots.length, 3)
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    arrows: false,
-                    slidesToShow: Math.min(state?.slideDots.length, 2)
-                }
-            },
-            {
-                breakpoint: 400,
-                settings: {
-                    arrows: false,
-                    slidesToShow: Math.min(state?.slideDots.length, 1)
-                }
-            }
-        ]
     };
     return (
         <>
@@ -107,22 +69,6 @@ function ListingDetailsGallery({data}) {
                         )
                     })}
                 </Slider>
-
-                {state.slideDots.length > 1 && <div className="gallery-carousel-dots">
-                    <Slider
-                        {...settingsThumbs}
-                        asNavFor={nav1}
-                        ref={slider => (setSlider2(slider))}
-                    >
-                        {state?.slideDots.map((slide, i) => {
-                            return (
-                                <div key={i}>
-                                    <img src={slide.img} alt=""/>
-                                </div>
-                            )
-                        })}
-                    </Slider>
-                </div>}
             </>}
         </>
     );

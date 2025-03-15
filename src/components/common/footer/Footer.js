e8533cb9c55d6009c3560e83818db550be3cb5cd
prageethmilan <prageethmilan1999@gmail.com>
2025-03-15 18:53:35 +0530

    remove commented components

--- src/components/common/footer/Footer.js
+++ src/components/common/footer/Footer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Copyright from "./Copyright";
 import FooterLeftWidget from "./FooterLeftWidget";
-import FooterQuickLinkWidget from "./FooterQuickLinkWidget";
-import FooterCategoryWidget from "./FooterCategoryWidget";
 import FooterContactWidget from "./FooterContactWidget";
 import sectiondata from "../../../store/store";
 
@@ -17,15 +15,13 @@ function Footer() {
                 <div className="box-icon"></div>
                 <div className="box-icon"></div>
                 <div className="container">
-                    <div className="row">
+                    <div className="row d-flex justify-content-around">
                         <FooterLeftWidget footerleftcnts={sectiondata.footerdata}/>
-                        <FooterQuickLinkWidget footerquicklink={sectiondata.footerdata.footerquicklinks} />
-                        <FooterCategoryWidget footercategory={sectiondata.footerdata.footercategories} />
-                        <FooterContactWidget footercontact={sectiondata.footerdata.footercontact} />
+                        <FooterContactWidget footercontact={sectiondata.footerdata.footercontact}/>
                     </div>
 
                     {/* Copyright */}
-                    <Copyright />
+                    <Copyright/>
                 </div>
             </section>
         </>
